3b25719a31a88579a4b1cfc457fa2c368ce21d6a
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-03-22 11:16:44 -0500

    week 1 added

---
+++ week1/apples.py
@@ -0,0 +1,6 @@
+applePrice = "$ 0.50"
+name = input("Please enter your name ")
+print("Hello " + name +" did you know apples are currently " + applePrice )
+amount=input("Would you like to buy some apples? Please enter how many? ")
+amount = int(amount)
+print("Thank you " + name + " for you purchase of " +str(amount) + " apples at the cost of " + applePrice + " each")
