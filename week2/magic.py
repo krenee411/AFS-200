9d9dc8931c4bfbed329bb504411e72c749a6ef8d
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-03-29 18:06:00 -0500

    SalesPrice corrected

--- week2/magic.py
+++ week2/magic.py
@@ -1,18 +1,12 @@
 # Prompt the user to input a number.
-from msilib import type_string
-
-
 FirstNumber = int(input("Please enter a number: "))
 # Multiple this number by 3.
 multiNumber = (FirstNumber * 3)
 # Add 6 to the number
 thenAdd = (multiNumber + 6)
-
 # Divide the new number by 3.
-thenDivide= (thenAdd / 3)
-
+thenDivide= (thenAdd // 3)
 # Subtract the number from step 1 from the answer in step 4.
 thenSubtract = (thenDivide - FirstNumber)
-
 # Display the results as an integer.  The results should always be 2. 
-print(int(thenSubtract))
+print(thenSubtract)
--- week2/saleprice.py
+++ week2/saleprice.py
@@ -1,21 +1,24 @@
-
 # Prompt the user to enter a description for a product.
 product = input("Please enter the description: ")
+
 # Prompt the user to enter the quantity being purchased. 
 amount = input(f"How many {product}'s would you like to order? ")
+
 # Store the value as an integer.
 amount = int(amount)
+
 # Prompt the user to enter the regular price of the product. This value must be stored as a float.
 regularAmount= input("What is the regular price? ")
 regularAmount = float(regularAmount)
+
 # All of the products over $19.99 are 15% OFF
 # All of the products over $39.99 are 25% OFF
 print("Your Receipt")
 print("----------------")
-if regularAmount <= 19.98:
+if regularAmount <= 19.99:
     newAmount = regularAmount 
     print(f"{amount} {product}'s @ {newAmount:.2f} each ")
-elif regularAmount > 19.99 and regularAmount <= 39.98 :
+elif regularAmount > 19.99 and regularAmount <= 39.99 :
     fifteen= regularAmount * .15
     newAmount = regularAmount - fifteen
     print(f"{amount} {product}'s @ {newAmount:.2f} each")
@@ -28,14 +31,18 @@ else:
     
 # Calculate the sales tax on the total purchase.  
 # Assume a state sales tax rate of 6.5%.  
-salesTax = 6.50
+salesTax = (newAmount * amount) * .065
+
 print(f"Sales Tax : ${salesTax:.2f}")
+
 # The rate should be calculated on the total price of the products after discount savings.
 # Store this value as float in a variable.
 totalWithDiscount = (newAmount* amount) + salesTax
 totalWithDiscount = float(totalWithDiscount)
+
 # Display the total amount due from the customer.
-print(f"Total amount due : ${totalWithDiscount:.2f}")
+print(f"Total amount due : ${totalWithDiscount:,.2f}")
+
 # Format the output as a fixed point number with two-decimal places, a comma as a thousand separator and the dollar sign.
 
 #   *****IM A BIT LOST ON THIS PART!*****
@@ -43,8 +50,10 @@ print(f"Total amount due : ${totalWithDiscount:.2f}")
 # Display the total amount saved.
 ogPrice = regularAmount * amount
 noDiscount = regularAmount == newAmount
-youSaved = ogPrice - totalWithDiscount
+youSaved = ogPrice - (amount * newAmount)
 if noDiscount :
-    print("Thank you for shopping with us")
+    print("Thank you for shopping with us!")
 else :
     print(f"You saved ${youSaved:.2f} today!")
+
+
