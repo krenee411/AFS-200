fdb87ce5a7c63e5fae7516409706d0d7247ed342
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-04-28 10:36:30 -0500

    Addressbook updated

--- week5/addressbook/app.py
+++ week5/addressbook/app.py
@@ -1,13 +1,12 @@
 from flask import Flask, request, render_template
 
 import json
-import requests
 import addressbook
 from addressbook import addressList
 
 
 addressesInList = []
-addressesInList.append(addressbook.Contact(1, "Kelci", "REnee", 1234567890, "kelci@kelci.com", ":)"))
+# addressesInList.append(addressbook.Contact(1, "Kelci", "REnee", 1234567890, "kelci@kelci.com", ":)"))
 
 
 app = Flask(__name__)
@@ -18,22 +17,14 @@ def home():
     return render_template('home.html')
 
 @app.route("/index", methods=["GET", "POST"])
+@app.route("/search", methods=["GET", "POST"])
 def index():
         if request.method == "POST":
-            first_Name = request.form.get("fName")
-            last_Name = request.form.get("lName")
-            phone_number = request.form.get("phone")
-            email_address = request.form.get("email")
-            dig_photo = request.form.get("photo")
-            
-            contactInfo = f"{first_Name} {last_Name}: {phone_number}: {email_address} {dig_photo}"
-            addressesInList.append(contactInfo)
-            return f"{first_Name} {last_Name}: {phone_number}: {email_address} {dig_photo}"
-            
-        else:
             searchStr = request.form.get("search")
             searchStr = str(searchStr)
             myList = addressList.findAllMatching(searchStr)
+        else:
+            myList = []
             return render_template('index.html',results = myList)
 
 
@@ -50,18 +41,9 @@ def myAddresseList():
         addressesInList.append(contactInfo)
         return f"{first_Name} {last_Name}: {phone_number}: {email_address} {dig_photo}"
     else:
-        # return json.dumps([item.__dict__ for item in addressesInList])
-        # print(addressList.showList())
         myList = addressList.showList()
-        # print("Hi", myList)
         
         return render_template('addressBook.html', results = myList)
-
-# @app.route('/contacts')
-# def contacts():
-#     myList = myAddresseList()
-#     print(myList)
-#     return render_template('addressBook.html')
 
 
 if __name__ == "__main__":
