91a244f7145131f638851b588b0efbc4825909d5
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-04-28 17:43:04 -0500

    Django Address Book added

---
+++ week6/contacts/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+add_name = "contact"
+urlpatterns = [
+    
+    path('', views.show_all_contacts ),
+    path('<int:contactid>',  views.show_contact ),    
+]
