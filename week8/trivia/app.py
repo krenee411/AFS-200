bc1e77dffa43d1b4e007ba934390c30aaad2c3f4
krenee411 <kelci.morgan@bryanuniversity.edu>
2022-05-05 13:32:00 -0500

    imcomplete

--- week8/trivia/app.py
+++ week8/trivia/app.py
@@ -10,12 +10,16 @@ app = Flask(__name__)
 
 questionList = TriviaGame()
 questionList.getData(3,20)
+
 @app.route("/",methods=["GET"])
 def home():
-    return render_template('questionPage.html',results = questionList.getAllQuestions() )
+    return render_template('questionPage.html',questions=questionList.getAllQuestions() )
 
 
-
+@app.route("/score", methods=['POST'])
+def score():
+    user_answer = request.form.get('1')
+    return render_template('answers.html', answers=questionList.getAllQuestions(), userAnswer=user_answer)
 
 if __name__ == "__main__":
     app.run()
